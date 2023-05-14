f4419357646fd24d41df74df1c7208b63fe3446f
Niels Kjær Ersbøll <nker@dtu.dk>
2023-05-15 00:01:33 +0200

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require('/home/peetz/Documents/GitHub/OpenLeakedDocs/node_modules/infima/dist/css/default/default.css'),
-  require('/home/peetz/Documents/GitHub/OpenLeakedDocs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/peetz/Documents/GitHub/OpenLeakedDocs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/peetz/Documents/GitHub/OpenLeakedDocs/src/css/custom.css'),
-  require('/home/peetz/Documents/GitHub/OpenLeakedDocs/node_modules/docusaurus-theme-openapi-docs/lib/theme/styles.css'),
-];
